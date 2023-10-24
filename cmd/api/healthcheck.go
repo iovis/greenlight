5a52f208752bccaf603f230440f70a5e38e18226
David Marchante <davidmarchan@gmail.com>
2023-10-23 21:46:23 -0400

    feat: Respond with JSON in healthcheck

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,12 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "status: available\n")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	response, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and couldn't process your request", http.StatusInternalServerError)
+		return
+	}
+
+	response = append(response, '\n')
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write(response)
 }
