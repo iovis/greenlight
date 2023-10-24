5d7a2626fcc3058ccc3eca47b37b8d11debc9014
David Marchante <davidmarchan@gmail.com>
2023-10-23 22:10:18 -0400

    feat: Extract helper for writing JSON

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -12,16 +11,9 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		"version":     version,
 	}
 
-	response, err := json.Marshal(data)
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Error(err.Error())
 		http.Error(w, "The server encountered a problem and couldn't process your request", http.StatusInternalServerError)
-		return
 	}
-
-	response = append(response, '\n')
-
-	w.Header().Set("Content-Type", "application/json")
-
-	w.Write(response)
 }
